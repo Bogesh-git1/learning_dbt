1c94341d8160238bdb4e6ed008f60db6afc68bdd
Bogesh reddy <Bogesh770@gmail.com>
2025-05-23 11:32:23 +0000

    commit-5

--- models/example/third_model.sql
+++ models/example/third_model.sql
@@ -1,5 +1,13 @@
-create table third_model (
-    id int,
-    name varchar,
-    salary int
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 2 as id
+    union all
+    select null_if as id
+
 )
+
+select *
+from source_data
